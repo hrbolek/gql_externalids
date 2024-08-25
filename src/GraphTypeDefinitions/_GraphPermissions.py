cfbea16f0473eced0ac5a52c3e2891ba33effaca
Hrbolek <alexandr.stefek@unob.cz>
2024-08-25 21:55:32 +0200

    new Strawberry

--- src/GraphTypeDefinitions/_GraphPermissions.py
+++ src/GraphTypeDefinitions/_GraphPermissions.py
@@ -285,7 +285,7 @@ from src.Dataloaders import (
     getUserFromInfo
     )
 
-from strawberry.types import StrawberryList
+from strawberry.types.base import StrawberryList
 class OnlyForAuthentized(strawberry.permission.BasePermission):
     message = "User is not authenticated"
 
